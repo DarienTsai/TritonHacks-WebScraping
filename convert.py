0591ac1daa823033ee19c271a0c89ca4a84a1047
Darien Tsai <smartdragondt@gmail.com>
2021-01-16 11:51:28 -0800

    refactor, combine

---
+++ convert.py
@@ -0,0 +1,16 @@
+"""
+[list of string prices (e.g. [$, $$])] => float
+"""
+def avg_price(prices):
+    sum = 0
+    for i in range(len(prices)):
+        sum += len(prices[i])
+    
+    return sum / len(prices)
+
+"""
+[list of numbers] => float
+"""
+def avg_rating(ratings):
+    return sum(ratings) / len(ratings)
+   
