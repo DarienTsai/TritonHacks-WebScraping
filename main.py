9db588c14186289c3392305263caf6284121e280
Darien Tsai <smartdragondt@gmail.com>
2021-01-23 10:20:46 -0800

    added basic error handling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Entrypoint to your webscraping starter kit
-import webbrowser # Used to open html page after scraping
-import os
+import os         # Used to open html page after scraping
+import webbrowser
+import sys        # Used to end program if error occurs
 
 from convert import avg_price, avg_rating
 from scraper import scrape_for
@@ -9,6 +10,7 @@ from generator import generate
 scrape_for("burger", "los angeles")
 
 if __name__ == "__main__":
+
     # Parse user input for queryngs
     print("Welcome to the location comparator app! Please enter your item to compare (e.g. hot dog):\n")
     item = input()
@@ -18,25 +20,33 @@ if __name__ == "__main__":
     place2 = input()
     print("Thank you! Generating report...\n\n")
 
-    # Make the scrapping requests (this may take a few seconds)
-    (prices1, stars1, images1) = scrape_for(item, place1)
-    (prices2, stars2, images2) = scrape_for(item, place2)
 
-    # calculate the averaged prices and stars of the separate locations
-    place1_avg_price = avg_price(prices1)
-    place1_avg_rating = avg_rating(stars1)
-    place2_avg_price = avg_price(prices2)
-    place2_avg_rating = avg_rating(stars2)
+    try:
+        # Make the scrapping requests (this may take a few seconds)
+        (prices1, stars1, images1) = scrape_for(item, place1)
+        (prices2, stars2, images2) = scrape_for(item, place2)
 
+        # calculate the averaged prices and stars of the separate locations
+        place1_avg_price = avg_price(prices1)
+        place1_avg_rating = avg_rating(stars1)
+        place2_avg_price = avg_price(prices2)
+        place2_avg_rating = avg_rating(stars2)
+
+    except:
+        print("Bad input, the program has ended")
+        sys.exit()
+
+    # Determine the cheaper and higher rated places
+    cheaper = place2 if place1_avg_price > place2_avg_price else place1
+    higher_rating = place2 if place2_avg_rating > place1_avg_rating else place1
+
+   
+    # Print results and generate html report
     print("In {p1}, the average rating for {it} is {ra} and the average price is {price}"
     .format(p1=place1, it=item, ra=avg_rating(stars1), price=avg_price(prices1)))
 
     print("In {p2}, the average rating for {it} is {ra} and the average price is {price}\n"
     .format(p2=place2, it=item, ra=avg_rating(stars2), price=avg_price(prices2)))
-
-    # Determine the cheaper and higher rated places
-    cheaper = place2 if place1_avg_price > place2_avg_price else place1
-    higher_rating = place2 if place2_avg_rating > place1_avg_rating else place1
 
     print("Based on this we can conclude that {} is cheaper...".format(cheaper))
     print("and {} is higher rated!".format(higher_rating))
